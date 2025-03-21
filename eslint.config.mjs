43e2276ddfa3ce9b4073bd761cbe1ed929b53e6e
Arjun Sharda <sharda.aj17@gmail.com>
2025-03-21 15:33:42 -0500

    s

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const compat = new FlatCompat();
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
@@ -18,5 +11,3 @@ const eslintConfig = [
     },
   },
 ];
-
-export default eslintConfig;
